9c8f932ac96210d97de7e5a889d9c296c62c0085
beata-honkisz@beata-honkisz-Latitude-3510.(none) <beatahonkisz@gmail.com>
2023-11-08 10:10:16 +0100

    added simple POM for login

--- demo-bank-tests/tests/login.spec.ts
+++ demo-bank-tests/tests/login.spec.ts
@@ -1,60 +1,57 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
+import { loginData } from '../test-data/login.data';
+import { LoginPage } from '../pages/login.page';
 
-test.describe("User login to Demobank", () => {
-
- 
-
+test.describe('User login to Demobank', () => {
   test.beforeEach(async ({ page }) => {
-    
     await page.goto('/');
   });
-  test("login with correct credentials", async ({ page }) => {
-    //Arrange
-    const userPassword = "12345678";
-    const userId = "testerLO";
-    const expectedUserName = "Jan Demobankowy";
 
-    //Act
+  test('successful login with correct credentials', async ({ page }) => {
+    // Arrange
+    const userId = loginData.userId;
+    const userPassword = loginData.userPassword;
+    const expectedUserName = 'Jan Demobankowy';
 
-    //await page.getByTestId('login-input').click();//niepotrzebny, bo fill załatwia sprawę
-    await page.getByTestId("login-input").fill(userId);
-    await page.getByTestId("password-input").fill(userPassword);
-    await page.getByTestId("login-button").click();
+    // Act
+    const loginPage = new LoginPage(page);
+    await loginPage.loginInput.fill(userId);
+    await loginPage.passwordInput.fill(userPassword);
+    await loginPage.loginButton.click();
 
-    //Assert
-    await expect(page.getByTestId("user-name")).toHaveText(expectedUserName);
+    // Assert
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
   });
-  test("unsuccesfull login with too short username", async ({ page }) => {
-    //Arrange
-    const errorLoginId = "error-login-id";
-    const userPassword = "12345678";
-    const userIncorrectId = "555";
 
-    //Act
-    await page.getByTestId("login-input").fill(userIncorrectId);
-    await page.getByTestId("login-input").blur();
-    await page.getByTestId("password-input").fill(userPassword);
+  test('unsuccessful login with too short username', async ({ page }) => {
+    // Arrange
+    const incorrectUserId = 'tester';
+    const expectedErrorMessage = 'identyfikator ma min. 8 znaków';
 
-    //Assert
-    await expect(page.getByTestId(errorLoginId)).toHaveText(
-      "identyfikator ma min. 8 znaków"
+    // Act
+    await page.getByTestId('login-input').fill(incorrectUserId);
+    await page.getByTestId('password-input').click();
+
+    // Assert
+    await expect(page.getByTestId('error-login-id')).toHaveText(
+      expectedErrorMessage
     );
   });
 
-  test("unsccesfull login with too short password", async ({ page }) => {
-    //Arrange
-    const userId = "testerLO";
-    const errorLoginPassword = "error-login-password";
-    const userIncorrectPassword = "123";
+  test('unsuccessful login with too short password', async ({ page }) => {
+    // Arrange
+    const userId = loginData.userId;
+    const incorrectPassword = '1234';
+    const expectedErrorMessage = 'hasło ma min. 8 znaków';
 
-    //Act:
-    await page.getByTestId("login-input").fill(userId);
-    await page.getByTestId("password-input").fill(userIncorrectPassword);
-    await page.getByTestId("password-input").blur(); //odwrotność focusa!!!!!!!!!
+    // Act
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(incorrectPassword);
+    await page.getByTestId('password-input').blur();
 
-    //Assert
-    await expect(page.getByTestId(errorLoginPassword)).toHaveText(
-      "hasło ma min. 8 znaków"
+    // Assert
+    await expect(page.getByTestId('error-login-password')).toHaveText(
+      expectedErrorMessage
     );
   });
 });
--- demo-bank-tests/tests/pulpit.spec.ts
+++ demo-bank-tests/tests/pulpit.spec.ts
@@ -1,80 +1,72 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
+import { loginData } from '../test-data/login.data';
+import { LoginPage } from '../pages/login.page';
 
-test.describe("Pulpit tests", () => {
-  const receiverId = "2";
-  
+test.describe('Pulpit tests', () => {
   test.beforeEach(async ({ page }) => {
-    const userId = "testerLO";
-    const userPassword = "12345678";
+    const userId = loginData.userId;
+    const userPassword = loginData.userPassword;
+
     await page.goto('/');
-    await page.getByTestId("login-input").fill(userId);
-    await page.getByTestId("password-input").fill(userPassword);
-    await page.getByTestId("login-button").click();
+    const loginPage = new LoginPage(page);
+    await loginPage.loginInput.fill(userId);
+    await loginPage.passwordInput.fill(userPassword);
+    await loginPage.loginButton.click();
   });
 
-  test("quick payment with correct data", async ({ page }) => {
-    //Arrange
-    const expectedTransferReceiver = "Chuck Demobankowy";
-    const transferAmount = "120";
-    const transferTitle = "Pizza";
+  test('quick payment with correct data', async ({ page }) => {
+    // Arrange
+    const receiverId = '2';
+    const transferAmount = '150';
+    const transferTitle = 'pizza';
+    const expectedTransferReceiver = 'Chuck Demobankowy';
 
-    //Act:
-    //await page.getByTestId('login-input').click();//niepotrzebny, bo fill załatwia sprawę
+    // Act
+    await page.locator('#widget_1_transfer_receiver').selectOption(receiverId);
+    await page.locator('#widget_1_transfer_amount').fill(transferAmount);
+    await page.locator('#widget_1_transfer_title').fill(transferTitle);
 
-    await page.locator("#widget_1_transfer_receiver").selectOption(receiverId);
-    await page.locator("#widget_1_transfer_amount").fill(transferAmount);
-    await page.locator("#widget_1_transfer_title").fill(transferTitle);
-    //await page.getByRole('button', { name: 'wykonaj' }).click();
-    await page.locator("#execute_btn").click();
-    await page.getByTestId("close-button").click();
+    await page.getByRole('button', { name: 'wykonaj' }).click();
+    await page.getByTestId('close-button').click();
 
-    //await page.getByRole('link', { name: 'Przelew wykonany! Chuck Demobankowy - 120,00PLN - Zwrot środków' }).click();
-
-    //Assert
-    await expect(page.locator("#show_messages")).toHaveText(
-      `Przelew wykonany! ${expectedTransferReceiver} - ${transferAmount},00PLN - ${transferTitle}`,
+    // Assert
+    await expect(page.locator('#show_messages')).toHaveText(
+      `Przelew wykonany! ${expectedTransferReceiver} - ${transferAmount},00PLN - ${transferTitle}`
     );
   });
 
-  test("quick top-up phone", async ({ page }) => {
-    //Arrange
-    const phoneNumber = "500 xxx xxx";
-    const topUpAmount = "12,00";
+  test('successful mobile top-up', async ({ page }) => {
+    // Arrange
+    const topUpReceiver = '500 xxx xxx';
+    const topUpAmount = '50';
+    const expectedMessage = `Doładowanie wykonane! ${topUpAmount},00PLN na numer ${topUpReceiver}`;
 
-    await page.locator("#widget_1_topup_receiver").selectOption(phoneNumber);
-    await page.locator("#widget_1_topup_amount").fill(topUpAmount);
-    await page.locator("#uniform-widget_1_topup_agreement span").click();
-    await page.getByRole("button", { name: "doładuj telefon" }).click();
-    await page.getByTestId("close-button").click();
+    // Act
+    await page.locator('#widget_1_topup_receiver').selectOption(topUpReceiver);
+    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
+    await page.locator('#uniform-widget_1_topup_agreement span').click();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
 
-    await expect(page.locator("#show_messages")).toHaveText(
-      `Doładowanie wykonane! ${topUpAmount}PLN na numer ${phoneNumber}`,
-    );
-    //await page.getByRole('link', { name: 'Doładowanie wykonane! 12,00PLN na numer 500 xxx xxx' }).click();
+    // Assert
+    await expect(page.locator('#show_messages')).toHaveText(expectedMessage);
   });
 
-  test("correct balance after successful top-up phone", async ({ page }) => {
-    //Arrange
-    const phoneNumber = "500 xxx xxx";
-    const topUpAmount = "50";
-    const initialBalance = await page.
-    locator('#money_value').innerText();
-    const expectedBalance = Number(initialBalance)-Number(topUpAmount)
+  test('correct balance after successful mobile top-up', async ({ page }) => {
+    // Arrange
+    const topUpReceiver = '500 xxx xxx';
+    const topUpAmount = '50';
+    const initialBalance = await page.locator('#money_value').innerText();
+    const expectedBalance = Number(initialBalance) - Number(topUpAmount);
 
+    // Act
+    await page.locator('#widget_1_topup_receiver').selectOption(topUpReceiver);
+    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
+    await page.locator('#uniform-widget_1_topup_agreement span').click();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
 
-    await page.locator("#widget_1_topup_receiver").selectOption(phoneNumber);
-    await page.locator("#widget_1_topup_amount").fill(topUpAmount);
-    await page.locator("#uniform-widget_1_topup_agreement span").click();
-    await page.getByRole("button", { name: "doładuj telefon" }).click();
-    await page.getByTestId("close-button").click();
-
-    await expect(page.locator("#money_value")).toHaveText(`${expectedBalance}`);
-
+    // Assert
+    await expect(page.locator('#money_value')).toHaveText(`${expectedBalance}`);
   });
-
-
-
-
-
-
 });
